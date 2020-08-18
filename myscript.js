c44464bfdf4efb6d2dd6a8c83555dbf694ffeaad
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-08-18 06:44:02 -0400

    got nav items to work

--- myscript.js
+++ myscript.js
@@ -1,10 +1,36 @@
-const open = document.querySelector('.navs-list-item');
-const drop = document.querySelector('.navs-list-dropdown');
+const openHome = document.querySelector('.navs-list-item.home');
+const dropHome = document.querySelector('.navs-list-dropdown.home');
+const openTwo = document.querySelector('.navs-list-item.listTwo');
+const dropTwo = document.querySelector('.navs-list-dropdown.listTwo');
+const openThree = document.querySelector('.navs-list-item.listThree');
+const dropThree = document.querySelector('.navs-list-dropdown.listThree');
 
 
-open.addEventListener('click', () => {
-  drop.classList.toggle('navs-list-dropdown');
+openHome.addEventListener("mouseover", () => {
+  dropHome.classList.remove('navs-list-dropdown');
 })
+
+openHome.addEventListener("mouseout", () => {
+  dropHome.classList.add('navs-list-dropdown');
+})
+
+openTwo.addEventListener("mouseover", () => {
+  dropTwo.classList.remove('navs-list-dropdown');
+})
+
+openTwo.addEventListener("mouseout", () => {
+  dropTwo.classList.add('navs-list-dropdown');
+})
+
+openThree.addEventListener("mouseover", () => {
+  dropThree.classList.remove('navs-list-dropdown');
+})
+
+openThree.addEventListener("mouseout", () => {
+  dropThree.classList.add('navs-list-dropdown');
+})
+
+
 
 // document.querySelector('.navs-list-dropdown').addEventListener('click', () => {
 //   console.log('yo')
